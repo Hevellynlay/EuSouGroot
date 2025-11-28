864b5346359e8a22115ab29250d373a9bc3ddd14
Hevellyn <celularhduarte@gmail.com>
2025-11-28 19:41:37 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    const string FraseParaSair = "tchau";
+
+    static void Main()
+    {
+        Console.WriteLine("--- Groot Chatbot ---");
+
+        string pergunta;
+        
+        do
+        {
+            Console.Write("Pergunta: ");
+            pergunta = Console.ReadLine()!;
+
+            // Resposta do Groot
+            Console.WriteLine($"Resposta: Eu sou Groot.");
+
+        } while (pergunta.Trim().ToLower() != FraseParaSair);
+
+        // Resposta final antes de sair
+        Console.WriteLine("Eu sou Groot!");
+    }
+}
+
